0a78a89901eeebd54ff925369f6d9ea10250f576
Marcin Kocot <marcin.kocot@jetbrains.com>
2026-02-13 13:06:23 +0100

    @nested repo

---
+++ nested/src/numberer/NumberPrinter.java
@@ -0,0 +1,13 @@
+package numberer;
+
+public class NumberPrinter {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      print(i);
+    }
+  }
+
+  private static void print(int i) {
+    System.out.println(i);
+  }
+}
